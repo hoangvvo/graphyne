6e11b874d613fc31c25eb3c93bb4353fde7ccd9d
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-04-14 13:08:53 -0400

    Update example

--- examples/with-express/index.js
+++ examples/with-express/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { GraphyneServer } = require('graphyne-express');
+const { GraphyneServer } = require('graphyne-server');
 const { makeExecutableSchema } = require('graphql-tools');
 
 const typeDefs = `
@@ -20,20 +20,23 @@ var schema = makeExecutableSchema({
 
 const graphyne = new GraphyneServer({
   schema,
-  context: () => ({ world: 'world' }),
+  context: (req, res) => ({ world: 'world' }),
 });
 
 const app = express();
-app.all('/graphql', graphyne.createHandler());
+
+const graphyneHandler = graphyne.createHandler({
+  path: '/graphql',
+  graphiql: {
+    path: '/___graphql',
+    defaultQuery: 'query { hello }',
+  },
+});
+
+// GraphQL API
+app.all('/graphql', graphyneHandler);
 // Use GraphiQL
-app.get(
-  '/___graphql',
-  graphyne.createHandler({
-    path: '/graphql', // This must be set for graphiql to work
-    graphiql: {
-      defaultQuery: 'query { hello }',
-    },
-  })
-);
+app.get('/___graphql', graphyneHandler);
+
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
