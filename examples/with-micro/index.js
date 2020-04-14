fe53b29992f9bcdad43092c6646c2d3bf1d09c39
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-04-14 15:34:53 -0400

    Add with-micro example

---
+++ examples/with-micro/index.js
@@ -0,0 +1,32 @@
+const { send } = require('micro');
+const { GraphyneServer } = require('graphyne-server');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: (obj, variables, context) => `Hello ${context.world}!`,
+  },
+};
+
+var schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const graphyne = new GraphyneServer({
+  schema,
+  context: (req, res) => ({ world: 'world' }),
+});
+
+module.exports = graphyne.createHandler({
+  // other options
+  onNoMatch: async (req, res) => {
+    const statusCode = 400;
+    send(res, statusCode, 'not found');
+  },
+});
