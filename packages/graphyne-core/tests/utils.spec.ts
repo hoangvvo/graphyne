528c8a81dfced853fbfa2add21acf8737f3a06ad
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-04-16 22:46:55 -0400

    Add test for utils

--- packages/graphyne-core/tests/utils.spec.ts
+++ packages/graphyne-core/tests/utils.spec.ts
@@ -1,53 +1,77 @@
-import { parseNodeRequest } from '../src/utils';
+import { parseNodeRequest, resolveMaybePromise } from '../src/utils';
 import { strict as assert } from 'assert';
 import { createServer } from 'http';
 import request from 'supertest';
 
-describe('parseNodeRequest', () => {
-  it('not parse if req.body has been parsed', (done) => {
-    const req = { body: { query: 1 } };
-    // @ts-ignore
-    parseNodeRequest(req, (err, parsedBody) => {
-      done(assert.deepStrictEqual(parsedBody, req.body));
-    });
-  });
-  it('errors body is malformed', async () => {
-    const server = createServer((req, res) => {
+describe('core utils', () => {
+  describe('parseNodeRequest', () => {
+    it('not parse if req.body has been parsed', (done) => {
+      const req = { body: { query: 1 } };
+      // @ts-ignore
       parseNodeRequest(req, (err, parsedBody) => {
-        if (err) res.statusCode = err.status;
-        res.end(JSON.stringify(parsedBody));
+        done(assert.deepStrictEqual(parsedBody, req.body));
       });
     });
-    await request(server)
-      .post('/graphql')
-      .send(`query { helloWorld`)
-      .set('content-type', 'application/json')
-      .expect(400);
+    it('errors body is malformed', async () => {
+      const server = createServer((req, res) => {
+        parseNodeRequest(req, (err, parsedBody) => {
+          if (err) res.statusCode = err.status;
+          res.end(JSON.stringify(parsedBody));
+        });
+      });
+      await request(server)
+        .post('/graphql')
+        .send(`query { helloWorld`)
+        .set('content-type', 'application/json')
+        .expect(400);
+    });
+    describe('do not parse POST body', () => {
+      it('with empty content type', async () => {
+        const server = createServer((req, res) => {
+          parseNodeRequest(req, (err, parsedBody) => {
+            res.end(JSON.stringify(parsedBody));
+          });
+        });
+        await request(server)
+          .post('/graphql')
+          .send(`query { helloWorld }`)
+          .set('content-type', '')
+          .expect('{}');
+      });
+      it('with invalid content-type', async () => {
+        const server = createServer((req, res) => {
+          parseNodeRequest(req, (err, parsedBody) => {
+            res.end(JSON.stringify(parsedBody));
+          });
+        });
+        await request(server)
+          .post('/graphql')
+          .send(`query { helloWorld }`)
+          .set('content-type', 'wat')
+          .expect('{}');
+      });
+    });
   });
-  describe('do not parse POST body', () => {
-    it('with empty content type', async () => {
-      const server = createServer((req, res) => {
-        parseNodeRequest(req, (err, parsedBody) => {
-          res.end(JSON.stringify(parsedBody));
-        });
-      });
-      await request(server)
-        .post('/graphql')
-        .send(`query { helloWorld }`)
-        .set('content-type', '')
-        .expect('{}');
+  describe('resolveMaybePromise', () => {
+    it('work with non-promise value', (done) => {
+      const val = 1;
+      resolveMaybePromise(val, (err, result) =>
+        done(assert.deepStrictEqual(result, 1))
+      );
     });
-    it('with invalid content-type', async () => {
-      const server = createServer((req, res) => {
-        parseNodeRequest(req, (err, parsedBody) => {
-          res.end(JSON.stringify(parsedBody));
-        });
+    it('work with promise value', (done) => {
+      const val = Promise.resolve(1);
+      resolveMaybePromise(val, (err, result) =>
+        done(assert.deepStrictEqual(result, 1))
+      );
+    });
+    it('errors on error', (done) => {
+      const fn = async () => {
+        throw new Error();
+      };
+      resolveMaybePromise(fn(), (err) => {
+        done(assert(err));
       });
-      await request(server)
-        .post('/graphql')
-        .send(`query { helloWorld }`)
-        .set('content-type', 'wat')
-        .expect('{}');
     });
   });
 });
