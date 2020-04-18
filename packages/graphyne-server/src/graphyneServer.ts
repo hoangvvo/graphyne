9dccc2e6ee7a7391b46acfdc03adef66e59a1313
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-04-18 00:12:15 -0400

    refactor

--- packages/graphyne-server/src/graphyneServer.ts
+++ packages/graphyne-server/src/graphyneServer.ts
@@ -17,18 +17,19 @@ import { GraphQLError } from 'graphql';
 const DEFAULT_PATH = '/graphql';
 const DEFAULT_GRAPHIQL_PATH = '/___graphql';
 
+function sendResponse(res: ServerResponse, result: QueryResponse) {
+  const { status, body, headers } = result;
+  for (const key in headers) {
+    const headVal = headers[key];
+    if (headVal) res.setHeader(key, headVal);
+  }
+  res.statusCode = status;
+  res.end(body);
+}
+
 export class GraphyneServer extends GraphyneServerBase {
   constructor(options: Config) {
     super(options);
-  }
-
-  sendResponse(res: ServerResponse, { status, body, headers }: QueryResponse) {
-    for (const key in headers) {
-      const headVal = headers[key];
-      if (headVal) res.setHeader(key, headVal);
-    }
-    res.statusCode = status;
-    res.end(body);
   }
 
   createHandler(options?: HandlerConfig): RequestListener | any {
@@ -60,68 +61,61 @@ export class GraphyneServer extends GraphyneServerBase {
       // Parse req.url
       const pathname = req.path || parseUrl(req, true).pathname;
       const path = options?.path ?? DEFAULT_PATH;
-      const self = this;
 
       return new Promise((resolve) => {
-        function sendResponse(
-          err: any,
-          { status, body, headers }: QueryResponse
-        ) {
-          for (const key in headers) {
-            const headVal = headers[key];
-            if (headVal) res.setHeader(key, headVal);
-          }
-          res.statusCode = status;
-          res.end(body);
-          resolve();
-        }
-
-        function handleRequest(err: any, parsedBody: QueryBody | undefined) {
-          if (err) {
-            res.statusCode = err.status || 500;
-            return res.end(JSON.stringify(err));
-          }
-          const queryParams = req.query || parseUrl(req, true).query;
-          const { query, variables, operationName } = getGraphQLParams({
-            queryParams: queryParams || {},
-            body: parsedBody,
-          });
-          const contextFn = self.options.context;
+        if (pathname === path) {
+          // serve GraphQL
+          const contextFn = this.options.context;
           const contextVal =
             (typeof contextFn === 'function'
               ? contextFn(...args)
               : contextFn) || {};
-          resolveMaybePromise(contextVal, (err, context) => {
-            if (err) {
-              res.statusCode = err.status || 500;
-              return res.end(
-                JSON.stringify({
-                  errors: [
-                    new GraphQLError(`Context creation failed: ${err.message}`),
-                  ],
-                })
-              );
-            }
-            self.runQuery(
-              {
-                query,
-                context,
-                variables,
-                operationName,
-                http: {
-                  request: req,
-                  response: res,
+          parseNodeRequest(req, (err, parsedBody) => {
+            if (err)
+              return sendResponse(res, {
+                status: err.status || 500,
+                body: JSON.stringify(err),
+                headers: {},
+              });
+            const queryParams = req.query || parseUrl(req, true).query;
+            const { query, variables, operationName } = getGraphQLParams({
+              queryParams: queryParams || {},
+              body: parsedBody,
+            });
+            resolveMaybePromise(contextVal, (err, context) => {
+              if (err)
+                return sendResponse(res, {
+                  status: err.status || 500,
+                  body: JSON.stringify({
+                    errors: [
+                      // TODO: More context
+                      new GraphQLError(
+                        `Context creation failed: ${err.message}`
+                      ),
+                    ],
+                  }),
+                  headers: { 'content-type': 'application/json' },
+                });
+              this.runQuery(
+                {
+                  query,
+                  context,
+                  variables,
+                  operationName,
+                  http: {
+                    request: req,
+                    response: res,
+                  },
                 },
-              },
-              sendResponse
-            );
+                (err, result) => {
+                  sendResponse(res, result);
+                  resolve();
+                }
+              );
+            });
           });
-        }
-
-        // serve GraphQL
-        if (pathname === path) parseNodeRequest(req, handleRequest);
-        // serve GraphiQL
-        else if (options?.graphiql) {
+        } else if (options?.graphiql) {
+          // serve GraphiQL
           const graphiql = options.graphiql;
           const graphiqlPath =
             (typeof graphiql === 'object' && graphiql.path) ||
@@ -129,20 +123,21 @@ export class GraphyneServer extends GraphyneServerBase {
           if (pathname === graphiqlPath) {
             const defaultQuery =
               typeof graphiql === 'object' ? graphiql.defaultQuery : undefined;
-            sendResponse(null, {
+            sendResponse(res, {
               status: 200,
               body: renderGraphiQL({ path, defaultQuery }),
               headers: {
                 'content-type': 'text/html; charset=utf-8',
               },
             });
+            resolve();
           }
-        }
-        // onNoMatch
-        else {
-          if (options?.onNoMatch) resolve(options.onNoMatch(...args));
+        } else {
+          // onNoMatch
+          if (options?.onNoMatch) options.onNoMatch(...args);
           else
-            sendResponse(null, { status: 404, body: 'not found', headers: {} });
+            sendResponse(res, { status: 404, body: 'not found', headers: {} });
+          resolve();
         }
       });
     };
