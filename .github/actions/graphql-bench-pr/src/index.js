c070080f4effb1ba5bb553425f03dc54d51a75ea
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-05-04 03:41:11 -0400

    Make sure PR benchmark runs properly (#44)
    * t
    
    * run
    
    * dump

--- .github/actions/graphql-bench-pr/src/index.js
+++ .github/actions/graphql-bench-pr/src/index.js
@@ -1,93 +1,115 @@
-const { promisify } = require('util');
-const { exec: execSync } = require('child_process');
-const exec = promisify(execSync);
-const { join } = require('path');
+const { promisify } = require("util")
+const { exec: execSync } = require("child_process")
+const exec = promisify(execSync)
+const { join } = require("path")
 const {
   promises: { readdir, writeFile },
   readFileSync,
-} = require('fs');
-const core = require('@actions/core');
-const github = require('@actions/github');
+} = require("fs")
+const core = require("@actions/core")
+const github = require("@actions/github")
 
-const repoToken = core.getInput('repoToken');
-const octokit = new github.GitHub(repoToken);
+const repoToken = core.getInput("repoToken")
+const octokit = new github.GitHub(repoToken)
 
 const {
   payload: { pull_request: pullRequest },
-} = github.context;
+} = github.context
 
-const cwd = process.env.GITHUB_WORKSPACE;
+const cwd = process.env.GITHUB_WORKSPACE
 
-const issueNumber = pullRequest.number;
-const baseRef = pullRequest.base.repo.ref || 'master';
-const baseRepo = pullRequest.base.repo.full_name;
-const prRepo = pullRequest.head.repo.full_name;
-const prRef = pullRequest.head.ref;
+const issueNumber = pullRequest.number
+const baseRef = pullRequest.base.repo.ref || "master"
+const baseRepo = pullRequest.base.repo.full_name
+const prRepo = pullRequest.head.repo.full_name
+const prRef = pullRequest.head.ref
 
 async function prepareRepo(repo, ref, outDir) {
-  console.log(cwd);
+  console.log(cwd)
   // Clone the repo and do yarn install
-  const url = `https://github.com/${repo}`;
-  await exec(`rm -rf ${outDir}`);
-  await exec(`git clone ${url} ${outDir}`);
-  await exec(`cd ${outDir} && git checkout ${ref}`);
+  const url = `https://github.com/${repo}`
+  await exec(`rm -rf ${outDir}`)
+  await exec(`git clone ${url} ${outDir}`)
+  await exec(`cd ${outDir} && git checkout ${ref}`)
   // Yarn link benchmark dependencies to packages
-  const pkgPath = join(cwd, outDir, 'package.json');
-  const package = JSON.parse(readFileSync(pkgPath).toString());
-  package.workspaces.push('bench');
-  await writeFile(pkgPath, JSON.stringify(package, null, '  '));
-  // yarn/issues/5500
-  await exec(`cd ${outDir} && rm yarn.lock && yarn install --silent`);
+
+  const f1 = async () => {
+    const pkgPath = join(cwd, outDir, "package.json")
+    const package = JSON.parse(readFileSync(pkgPath).toString())
+    package.workspaces.push("bench")
+    await writeFile(pkgPath, JSON.stringify(package, null, "  "))
+    await exec(`cd ${outDir} && rm yarn.lock && yarn install --silent`)
+  }
+
+  const f2 = async () => {
+    const filePath = join(
+      cwd,
+      outDir,
+      "bench",
+      "benchmarks",
+      "graphyne-server.js"
+    )
+    const file = readFileSync(filePath).toString()
+    await writeFile(
+      filePath,
+      file.replace(
+        `require('graphyne-server')`,
+        `require('../../packages/graphyne-server')`
+      )
+    )
+  }
+
+  await Promise.all([f1(), f2()])
 }
 
 async function runManyBenches(dir, packages) {
   // Run benchmark
-  let packagesArgs = packages.join(' ');
+  let packagesArgs = packages.join(" ")
   const benchCmd = `node ${dir}/bench/bench -c 100 -d 10 -p 10 --packages ${packagesArgs} --silent`
-  await exec(benchCmd);
+  await exec(benchCmd)
 }
 
 async function getStats(repo, ref) {
-  const dir = repo.replace('/', '-');
-  await prepareRepo(repo, ref, dir);
-  await runManyBenches(dir, ['graphyne-server']);
+  const dir = repo.replace("/", "-")
+  await prepareRepo(repo, ref, dir)
+  await runManyBenches(dir, ["graphyne-server"])
   // Get the result
-  const resultsPath = join(cwd, dir, 'bench', 'results');
-  const resultObj = {};
-  (await readdir(resultsPath)).map((file) => {
-    const content = readFileSync(`${resultsPath}/${file}`);
-    const data = JSON.parse(content.toString());
+  const resultsPath = join(cwd, dir, "bench", "results")
+  const resultObj = {}
+  ;(await readdir(resultsPath)).map((file) => {
+    const content = readFileSync(`${resultsPath}/${file}`)
+    const data = JSON.parse(content.toString())
     const {
       title,
       requests: { average: requests },
       latency: { average: latency },
       throughput: { average: throughput },
-    } = data;
+    } = data
     resultObj[title] = {
       requests: requests.toFixed(1),
       latency: latency.toFixed(2),
       throughput: (throughput / 1024 / 1024).toFixed(2),
-    };
-  });
-  return resultObj;
+    }
+  })
+  return resultObj
 }
 
-(async () => {
-  const statsBase = await getStats(baseRepo, baseRef);
-  const statsPR = await getStats(prRepo, prRef);
+;(async () => {
+  const statsBase = await getStats(baseRepo, baseRef)
+  const statsPR = await getStats(prRepo, prRef)
 
   let message = `# Benchmarks from current PR
 
 Hey, @${pullRequest.user.login}. I ran some benchmarks on this PR.
-`;
+`
 
   for (let key of Object.keys(statsBase)) {
-    const { requests, latency, throughput } = statsBase[key];
+    const { requests, latency, throughput } = statsBase[key]
     const {
       requests: prRequests,
       latency: prLatency,
       throughput: prThroughput,
-    } = statsPR[key];
+    } = statsPR[key]
     message += `
 
 **${key}**
@@ -95,26 +117,26 @@ Hey, @${pullRequest.user.login}. I ran some benchmarks on this PR.
 | | ${baseRepo} ${baseRef} | ${prRepo} ${prRef} | Change |
 | --- | --- | --- | --- |
 | Requests/s | ${requests} | ${prRequests} | ${
-      prRequests < requests ? '⚠️ ' : ' +'
+      prRequests < requests ? "⚠️ " : " +"
     }${(prRequests - requests).toFixed(1)} |
-| Latency | ${latency} | ${prLatency} | ${prLatency > latency ? '⚠️ +' : ' '}${
-      (prLatency - latency).toFixed(2)
-    } | 
+| Latency | ${latency} | ${prLatency} | ${prLatency > latency ? "⚠️ +" : " "}${(
+      prLatency - latency
+    ).toFixed(2)} | 
 | Throughput/Mb | ${throughput} | ${prThroughput} | ${
-      prThroughput < throughput ? '⚠️ ' : ' +'
+      prThroughput < throughput ? "⚠️ " : " +"
     }${(prThroughput - throughput).toFixed(2)} |
-`;
+`
   }
 
   message += `
   
 Thanks for the contribution :+1:`
 
-  const [owner, repo] = baseRepo.split('/');
+  const [owner, repo] = baseRepo.split("/")
   await octokit.issues.createComment({
     owner,
     repo,
     issue_number: issueNumber,
     body: message,
-  });
-})();
+  })
+})()
