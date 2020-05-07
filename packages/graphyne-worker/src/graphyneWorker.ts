06b1cfa13874e0269341421952bf7691ddc7edbd
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-05-06 20:30:20 -0400

    Add test for graphyne worker

--- packages/graphyne-worker/src/graphyneWorker.ts
+++ packages/graphyne-worker/src/graphyneWorker.ts
@@ -56,25 +56,30 @@ export class GraphyneWorker extends GraphyneCore {
     }
 
     return new Promise((resolve) => {
+      const variablesParam = url.searchParams.get('variables');
       this.runQuery(
         {
-          query: url.searchParams.get('query') || body?.query,
+          query: body?.query || url.searchParams.get('query') || undefined,
           context,
           variables:
-            JSON.parse(url.searchParams.get('variables') || '') ||
-            body?.variables,
+            body?.variables || (variablesParam && JSON.parse(variablesParam)),
           operationName:
-            url.searchParams.get('operationName') || body?.operationName,
+            body?.operationName ||
+            url.searchParams.get('operationName') ||
+            undefined,
           httpRequest: {
             method: request.method,
           },
         },
-        ({ status, body, headers }) => new Response(body, { status, headers })
+        ({ status, body, headers }) =>
+          resolve(new Response(body, { status, headers }))
       );
     });
   }
 
-  createHandler(options: HandlerConfig) {
+  // @ts-ignore
+  createHandler(options: HandlerConfig): (event: FetchEvent) => void {
+    // @ts-ignore
     return (event: FetchEvent) => {
       const url = new URL(event.request.url);
       event.respondWith(this.handleRequest(event.request, url));
