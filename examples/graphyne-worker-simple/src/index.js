7c62c436e6af0ad0f9292681c46545323e1b9767
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-06-08 19:59:48 -0400

    Use parcel

--- examples/graphyne-worker-simple/src/index.js
+++ examples/graphyne-worker-simple/src/index.js
@@ -1,33 +1,6 @@
-import { GraphyneWorker } from 'graphyne-worker';
-import { makeExecutableSchema } from 'graphql-tools';
+function createWorker() {
+  const worker = new Worker("./worker.js");
+  return worker;
+}
 
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
-const resolvers = {
-  Query: {
-    hello: (obj, variables, context) => `Hello ${context.world}!`,
-  },
-};
-
-var schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
-
-const graphyne = new GraphyneWorker({
-  schema,
-  context: () => ({ world: 'world' }),
-});
-
-global.addEventListener(
-  'fetch',
-  graphyne.createHandler({
-    path: '/graphql',
-    playground: {
-      path: '/playground',
-    },
-  })
-);
+createWorker();
