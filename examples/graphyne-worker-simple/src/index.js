630ab516eeed5bc649413ced111252ec8e6d0ec7
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-06-09 09:51:48 -0400

    Done example

--- examples/graphyne-worker-simple/src/index.js
+++ examples/graphyne-worker-simple/src/index.js
@@ -1,9 +1,24 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/worker.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
+  navigator.serviceWorker
+    .register('/worker.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function (error) {
+      console.log('Service worker registration failed, error:', error);
+    });
 }
+
+window.onload = () => {
+  document.querySelector('#query').onclick = () => {
+    fetch('/graphql?query={hello}')
+      .then((res) => res.json())
+      .then((json) => {
+        document.querySelector('#result').value = JSON.stringify(
+          json,
+          null,
+          '\t'
+        );
+      });
+  };
+};
