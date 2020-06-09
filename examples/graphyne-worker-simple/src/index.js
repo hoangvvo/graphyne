7b5a43580853f377a3980ee5027c274f8cfc93ff
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-06-08 20:40:35 -0400

    Register service worker

--- examples/graphyne-worker-simple/src/index.js
+++ examples/graphyne-worker-simple/src/index.js
@@ -1,6 +1,9 @@
-function createWorker() {
-  const worker = new Worker("./worker.js");
-  return worker;
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/worker.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
 }
-
-createWorker();
