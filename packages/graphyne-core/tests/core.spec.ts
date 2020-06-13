a8b9e4c81784219dc6637b9063f4d89889004b4c
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-06-13 16:51:12 -0400

    graphyne-core: ddd test case for cache and improve test

--- packages/graphyne-core/tests/core.spec.ts
+++ packages/graphyne-core/tests/core.spec.ts
@@ -1,21 +1,19 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import { GraphQLSchema } from 'graphql';
 import { strict as assert, deepStrictEqual } from 'assert';
-import {
-  GraphyneCore,
-  QueryRequest,
-  QueryResponse,
-  Config,
-  QueryBody,
-} from '../src';
+import { GraphyneCore, Config, QueryBody, QueryCache } from '../src';
+import { Lru } from 'tiny-lru';
 
 const schema = makeExecutableSchema({
   typeDefs: `
     type Query {
       hello(who: String!): String
       helloWorld: String
+      asyncHelloWorld: String
       helloRoot: String
       throwMe: String
+      asyncThrowMe: String
+      dangerousThrow: String
       helloContext: String
     }
     type Mutation {
@@ -25,10 +23,19 @@ const schema = makeExecutableSchema({
   resolvers: {
     Query: {
       hello: (obj, args) => args.who,
+      asyncHelloWorld: async (obj, args) => 'world',
       helloWorld: () => 'world',
       helloRoot: ({ name }) => name,
-      throwMe: async () => {
-        throw new Error('weeeeee');
+      throwMe: () => {
+        throw new Error('im thrown');
+      },
+      asyncThrowMe: async () => {
+        throw new Error('im thrown');
+      },
+      dangerousThrow: () => {
+        const err = new Error('oh no');
+        (err as any).systemSecret = '123';
+        throw err;
       },
       helloContext: (obj, args, context) => 'Hello ' + context.robot,
     },
@@ -38,56 +45,11 @@ const schema = makeExecutableSchema({
   },
 });
 
-function testCase(
-  queryRequest: QueryBody & {
-    context?: Record<string, any>;
-    httpMethod?: string;
-  },
-  expected: Partial<QueryResponse> | { body: (b: string) => boolean },
-  options?: Partial<Config>
-) {
-  if (!queryRequest.context) queryRequest.context = {};
-  if (!queryRequest.httpMethod) queryRequest.httpMethod = 'POST';
-  return new Promise((resolve, reject) => {
-    new GraphyneCore({
-      schema,
-      ...options,
-      // @ts-ignore
-    }).runHttpQuery(queryRequest, (result) => {
-      if (typeof expected.body === 'function') {
-        return expected.body(result.body) ? resolve() : reject('no match');
-      }
-      try {
-        deepStrictEqual(
-          {
-            headers: { 'content-type': 'application/json' },
-            status: 200,
-            ...expected,
-          },
-          result
-        );
-        resolve();
-      } catch (err) {
-        reject(err);
-      }
-    });
-  });
-}
-
 describe('graphyne-core', () => {
   it('throws if initializing instance with no option', () => {
     assert.throws(() => {
       // @ts-ignore
       new GraphyneCore();
-    });
-  });
-  it('throws if options.context is not a function or object', () => {
-    assert.throws(() => {
-      new GraphyneCore({
-        // @ts-ignore
-        context: 1,
-        schema,
-      });
     });
   });
   it('throws if schema is invalid', () => {
@@ -100,17 +62,68 @@ describe('graphyne-core', () => {
   });
 });
 
-describe('Operations', () => {});
-
 describe('HTTP Operations', () => {
-  it('allows regular request', () => {
-    return testCase(
+  type ExpectedBodyFn = (str: string) => boolean;
+
+  function testHttp(
+    queryRequest: QueryBody & {
+      context?: Record<string, any>;
+      httpMethod?: string;
+    },
+    expected: {
+      status?: number;
+      body: string | ExpectedBodyFn;
+      headers?: Record<string, string>;
+    },
+    options?: Partial<Config>
+  ) {
+    if (!queryRequest.context) queryRequest.context = {};
+    if (!queryRequest.httpMethod) queryRequest.httpMethod = 'POST';
+    return new Promise((resolve, reject) => {
+      new GraphyneCore({
+        schema,
+        ...options,
+        // @ts-ignore
+      }).runHttpQuery(queryRequest, (result) => {
+        if (typeof expected.body === 'function') {
+          // check using custom function
+          if (!expected.body(result.body))
+            throw new Error('actual body does not match body check function');
+          // already check body, no longer need
+          delete expected.body;
+          delete result.body;
+        }
+        try {
+          deepStrictEqual(
+            {
+              headers: { 'content-type': 'application/json' },
+              status: 200,
+              ...expected,
+            },
+            result
+          );
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  it('allows simple request', () => {
+    return testHttp(
       { query: 'query { helloWorld }' },
       { body: `{"data":{"helloWorld":"world"}}` }
     );
   });
+  it('allows simple request with async resolver', () => {
+    return testHttp(
+      { query: 'query { asyncHelloWorld }' },
+      { body: `{"data":{"asyncHelloWorld":"world"}}` }
+    );
+  });
   it('allows request with variables', () => {
-    return testCase(
+    return testHttp(
       {
         query: 'query helloWho($who: String!) { hello(who: $who) }',
         variables: { who: 'John' },
@@ -118,8 +131,8 @@ describe('HTTP Operations', () => {
       { body: `{"data":{"hello":"John"}}` }
     );
   });
-  it('errors when missing operation name request', async () => {
-    return testCase(
+  it('errors when missing operation name', async () => {
+    return testHttp(
       {
         query: `query helloJohn { hello(who: "John") }
       query helloJane { hello(who: "Jane") }
@@ -133,7 +146,7 @@ describe('HTTP Operations', () => {
     );
   });
   it('allows request with operation name', () => {
-    return testCase(
+    return testHttp(
       {
         query: `query helloJohn { hello(who: "John") }
         query helloJane { hello(who: "Jane") }
@@ -144,7 +157,7 @@ describe('HTTP Operations', () => {
     );
   });
   it('errors when missing query', () => {
-    return testCase(
+    return testHttp(
       { context: {} },
       {
         status: 400,
@@ -153,7 +166,7 @@ describe('HTTP Operations', () => {
     );
   });
   it('errors when sending a mutation via GET request', () => {
-    return testCase(
+    return testHttp(
       {
         query: `mutation sayHelloWho { sayHello(who: "Jane") }`,
         httpMethod: 'GET',
@@ -165,8 +178,8 @@ describe('HTTP Operations', () => {
       }
     );
   });
-  it('errors when sending GraphQL not via either GET or POST request', () => {
-    return testCase(
+  it('errors when sending via neither GET nor POST request', () => {
+    return testHttp(
       {
         query: `mutation sayHelloWho { sayHello(who: "Jane") }`,
         httpMethod: 'PUT',
@@ -178,14 +191,8 @@ describe('HTTP Operations', () => {
       }
     );
   });
-  it('allows sending a mutation via POST request', () => {
-    return testCase(
-      { query: `mutation sayHelloWho { sayHello(who: "Jane") }` },
-      { body: '{"data":{"sayHello":"Hello Jane"}}' }
-    );
-  });
-  it('allows runHttpQuery with a context', () => {
-    return testCase(
+  it('allows context value', () => {
+    return testHttp(
       {
         query: `{ helloContext }`,
         context: {
@@ -195,20 +202,20 @@ describe('HTTP Operations', () => {
       { body: '{"data":{"helloContext":"Hello R2-D2"}}' }
     );
   });
-  describe('allows defining options.rootValue', () => {
+  describe('allows options.rootValue', () => {
     const rootValue = {
       name: 'Luke',
     };
     // FIXME: need better test
-    it('with an object', () => {
-      return testCase(
+    it('as an object', () => {
+      return testHttp(
         { query: 'query { helloRoot }', httpMethod: 'GET' },
         { body: '{"data":{"helloRoot":"Luke"}}' },
         { rootValue }
       );
     });
-    it('with a function', () => {
-      return testCase(
+    it('as a function', () => {
+      return testHttp(
         { query: 'query { helloRoot }', httpMethod: 'GET' },
         { body: '{"data":{"helloRoot":"Luke"}}' },
         { rootValue: () => rootValue }
@@ -217,9 +224,10 @@ describe('HTTP Operations', () => {
   });
   describe('errors on validation errors', () => {
     it('when there are unknown fields', () => {
-      return testCase(
+      return testHttp(
         { query: `query { xinchao, hola, hello }` },
         {
+          status: 400,
           body: (str) => {
             const {
               errors: [err1, err2],
@@ -238,9 +246,10 @@ describe('HTTP Operations', () => {
       );
     });
     it('when missing required arguments', () => {
-      return testCase(
+      return testHttp(
         { query: `query { hello }` },
         {
+          status: 400,
           body: (str) => {
             const {
               errors: [err],
@@ -255,12 +264,13 @@ describe('HTTP Operations', () => {
       );
     });
     it('when arguments have incorrect types', async () => {
-      return testCase(
+      return testHttp(
         {
           query: 'query helloWho($who: String!) { hello(who: $who) }',
           variables: { who: 12 },
         },
         {
+          status: 200,
           body: (str) => {
             const {
               errors: [err],
@@ -275,9 +285,10 @@ describe('HTTP Operations', () => {
       );
     });
     it('when query is malformed', () => {
-      return testCase(
+      return testHttp(
         { query: 'query { helloWorld ' },
         {
+          status: 400,
           body: (str) => {
             const {
               errors: [err],
@@ -292,37 +303,128 @@ describe('HTTP Operations', () => {
       );
     });
   });
-  it('catches error thrown in resolver function', () => {
-    return testCase(
+  it('catches error in resolver function', () => {
+    return testHttp(
       { query: 'query { throwMe }' },
       {
         body: (str) => {
           const {
             errors: [err],
           } = JSON.parse(str);
-          assert.deepStrictEqual(err.message, 'weeeeee');
+          assert.deepStrictEqual(err.message, 'im thrown');
           return true;
         },
       }
     );
   });
-  it('allows custom formatError', () => {
-    return testCase(
-      { query: 'query { throwMe }' },
+  it('catches error in async resolver function', () => {
+    return testHttp(
+      { query: 'query { asyncThrowMe }' },
       {
         body: (str) => {
           const {
             errors: [err],
           } = JSON.parse(str);
-          assert.deepStrictEqual(err.message, 'Internal server error');
+          assert.deepStrictEqual(err.message, 'im thrown');
           return true;
-        },
-      },
-      {
-        formatError: (err) => {
-          return new Error('Internal server error');
         },
       }
     );
+  });
+  describe('allows format errors', () => {
+    it('using default formatError', () => {
+      return testHttp(
+        { query: 'query { dangerousThrow }' },
+        {
+          body: (str) => {
+            const {
+              errors: [err],
+            } = JSON.parse(str);
+            assert.deepStrictEqual(err.message, 'oh no');
+            // formatError will filter trivial prop
+            assert.deepStrictEqual(err.systemSecret, undefined);
+            return true;
+          },
+        }
+      );
+    });
+    it('using custom formatError', () => {
+      return testHttp(
+        { query: 'query { dangerousThrow }' },
+        {
+          body: (str) => {
+            const {
+              errors: [err],
+            } = JSON.parse(str);
+            assert.deepStrictEqual(err.message, 'Internal server error');
+            return true;
+          },
+        },
+        {
+          formatError: (err) => {
+            return new Error('Internal server error');
+          },
+        }
+      );
+    });
+  });
+  it('saves compiled query to cache', async () => {
+    const graphyne = new GraphyneCore({
+      schema,
+    });
+    const lru: Lru<QueryCache> = (graphyne as any).lru;
+    await new Promise((resolve) => {
+      graphyne.runHttpQuery(
+        {
+          query: '{ helloWorld }',
+          httpMethod: 'GET',
+          context: {},
+        },
+        resolve
+      );
+    });
+    assert(lru.has('{ helloWorld }'));
+  });
+  it('uses compiled query from cache', async () => {
+    const graphyne = new GraphyneCore({
+      schema,
+    });
+    const lru: Lru<QueryCache> = (graphyne as any).lru;
+    lru.set('{ helloWorld }', {
+      compiledQuery: {
+        query: () => ({ data: { cached: true } }),
+        stringify: JSON.stringify,
+      },
+      operation: 'query',
+      document: '' as any,
+    });
+    const { body } = await new Promise((resolve) => {
+      graphyne.runHttpQuery(
+        {
+          query: '{ helloWorld }',
+          httpMethod: 'GET',
+          context: {},
+        },
+        resolve
+      );
+    });
+    assert.deepStrictEqual(body, JSON.stringify({ data: { cached: true } }));
+  });
+  it('does not cache bad query', async () => {
+    const graphyne = new GraphyneCore({
+      schema,
+    });
+    const lru: Lru<QueryCache> = (graphyne as any).lru;
+    await new Promise((resolve) => {
+      graphyne.runHttpQuery(
+        {
+          query: '{ watt }',
+          httpMethod: 'GET',
+          context: {},
+        },
+        resolve
+      );
+    });
+    assert(lru.has('{ watt }') !== true);
   });
 });
