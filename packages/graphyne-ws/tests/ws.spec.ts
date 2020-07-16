f0cf1020a6e48f0e35e2d950d2e61f94d7c9fc29
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-07-16 17:51:56 -0400

    Fix and improve graphyne-ws test

--- packages/graphyne-ws/tests/ws.spec.ts
+++ packages/graphyne-ws/tests/ws.spec.ts
@@ -1,14 +1,14 @@
 import { wsHandler } from '../src';
 import { GraphyneWSOptions } from '../src/types';
 import { SubscriptionConnection } from '../src/connection';
-import { Graphyne, Config as GraphyneConfig } from '../../graphyne-core';
+import { Graphyne, Config as GraphyneConfig } from '../../graphyne-core/src';
 import { parseBody } from '../../graphyne-server/src/http/parseBody';
 import WebSocket from 'ws';
 import { strict as assert } from 'assert';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { PubSub } from 'graphql-subscriptions';
 import { createServer } from 'http';
-import request from 'supertest';
+import fetch from 'node-fetch';
 import { GraphQLError } from 'graphql';
 
 const pubsub = new PubSub();
@@ -17,6 +17,7 @@ const typeDefs = `
   type Notification {
     message: String
     dummy: String
+    DO_NOT_USE_THIS_FIELD: String
   }
 
   type Query {
@@ -52,6 +53,9 @@ const resolvers = {
   },
   Notification: {
     dummy: ({ message }) => message,
+    DO_NOT_USE_THIS_FIELD: () => {
+      throw new Error('I told you so');
+    },
   },
 };
 
@@ -59,6 +63,8 @@ const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
+
+let serverInit;
 
 async function startServer(
   options: { ws?: WebSocket } = {},
@@ -83,13 +89,38 @@ async function startServer(
     });
   });
   const wss = new WebSocket.Server({ server });
+  // We cross test different packages
+  // @ts-ignore
   wss.on('connection', wsHandler(graphyne, graphyneWsOptions));
   const client = WebSocket.createWebSocketStream(ws, {
     encoding: 'utf8',
     objectMode: true,
   });
   await new Promise((resolve) => server.listen(4000, resolve));
-  return { server, client, ws };
+  return (serverInit = { server, client, ws });
+}
+
+afterEach(function () {
+  if (!serverInit) return;
+  const { server, client } = serverInit;
+  client.end();
+  server.close();
+});
+
+function sendMessageMutation() {
+  return fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `mutation {
+        addNotification(message: "Hello World") {
+          message
+        }
+      }`,
+    }),
+  });
 }
 
 describe('graphyne-ws: wsHandler', () => {
@@ -102,9 +133,9 @@ describe('graphyne-ws: wsHandler', () => {
     );
     await new Promise((resolve) => {
       client.on('data', (chunk) => {
-        client.end();
-        server.close();
-        resolve(assert.deepStrictEqual(chunk, `{"type":"connection_ack"}`));
+        const json = JSON.parse(chunk);
+        assert.deepStrictEqual(json, { type: 'connection_ack' });
+        resolve();
       });
     });
   });
@@ -135,29 +166,22 @@ describe('graphyne-ws: wsHandler', () => {
       client.on('data', (chunk) => {
         const data = JSON.parse(chunk);
         if (data.type === 'connection_ack') {
-          return request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-              query: `
-            mutation {
-              addNotification(message: "Hello World") {
-                message
-              }
-            }
-          `,
-            })
-            .expect(`{"data":{"addNotification":{"message":"Hello World"}}}`)
-            .catch(reject);
+          return sendMessageMutation();
         }
         if (data.type === 'data') {
-          client.end();
-          server.close();
-          resolve(
-            assert.deepStrictEqual(
-              chunk,
-              `{"type":"data","id":1,"payload":{"data":{"notificationAdded":{"message":"Hello World","dummy":"Hello World"}}}}`
-            )
-          );
+          assert.deepStrictEqual(data, {
+            type: 'data',
+            id: 1,
+            payload: {
+              data: {
+                notificationAdded: {
+                  message: 'Hello World',
+                  dummy: 'Hello World',
+                },
+              },
+            },
+          });
+          resolve();
         }
       });
     });
@@ -167,8 +191,6 @@ describe('graphyne-ws: wsHandler', () => {
     const { server, client } = await startServer({ ws });
     await new Promise((resolve) =>
       ws.on('close', () => {
-        client.end();
-        server.close();
         resolve();
       })
     );
@@ -177,14 +199,14 @@ describe('graphyne-ws: wsHandler', () => {
     startServer().then(({ server, client, ws }) => {
       client.write(`{"type":"connection_init","payload":`);
       client.on('data', (chunk) => {
-        client.end();
-        server.close();
-        done(
-          assert.deepStrictEqual(
-            chunk,
-            `{"type":"error","payload":{"errors":[{"message":"Malformed message"}]}}`
-          )
-        );
+        const json = JSON.parse(chunk);
+        if (json.type === 'error') {
+          assert.deepStrictEqual(json, {
+            type: 'error',
+            payload: { message: 'Malformed message' },
+          });
+          done();
+        }
       });
     });
   });
@@ -197,17 +219,49 @@ describe('graphyne-ws: wsHandler', () => {
         },
       }
     ).then(({ server, client, ws }) => {
-      client.write(`{"type":"connection_init","payload":`);
+      client.write(
+        JSON.stringify({
+          type: 'connection_init',
+        })
+      );
+      client.write(
+        JSON.stringify({
+          id: 1,
+          type: 'start',
+          payload: {
+            query: `
+            subscription {
+              notificationAdded {
+                message
+                DO_NOT_USE_THIS_FIELD
+              }
+            }
+          `,
+          },
+        })
+      );
       client.on('data', (chunk) => {
-        client.end();
-        server.close();
-        // Override default error which is "Malformed message"
-        done(
-          assert.deepStrictEqual(
-            chunk,
-            `{"type":"error","payload":{"errors":[{"message":"Internal server error"}]}}`
-          )
-        );
+        const json = JSON.parse(chunk);
+        if (json.type === 'connection_ack') {
+          sendMessageMutation();
+        }
+        if (json.type === 'data') {
+          assert.deepStrictEqual(json, {
+            id: 1,
+            type: 'data',
+            payload: {
+              data: {
+                notificationAdded: {
+                  DO_NOT_USE_THIS_FIELD: null,
+                  message: 'Hello World',
+                },
+              },
+              // Override "I told you so" error
+              errors: [{ message: 'Internal server error' }],
+            },
+          });
+          done();
+        }
       });
     });
   });
@@ -229,19 +283,20 @@ describe('graphyne-ws: wsHandler', () => {
     );
     await new Promise((resolve, reject) => {
       client.on('data', (chunk) => {
-        if (chunk === `{"type":"connection_ack"}`) return;
-        server.close();
-        client.end();
-        resolve(
-          assert.deepStrictEqual(
-            chunk,
-            `{"type":"error","id":1,"payload":{"errors":[{"message":"Must provide query string."}]}}`
-          )
-        );
+        const json = JSON.parse(chunk);
+        if (json.type === 'error') {
+          assert.deepStrictEqual(json, {
+            type: 'error',
+            id: 1,
+            payload: { message: 'Must provide query string.' },
+          });
+          resolve();
+        }
       });
     });
   });
-  it('errors on operation other than subscription', async function () {
+  it('resolves also queries and mutations', async function () {
+    // We can also add a Query test just to be sure but Mutation one only should be sufficient
     const { server, client } = await startServer();
     client.write(
       JSON.stringify({
@@ -264,16 +319,22 @@ describe('graphyne-ws: wsHandler', () => {
       })
     );
     await new Promise((resolve, reject) => {
+      let resolved = false;
       client.on('data', (chunk) => {
-        if (chunk === `{"type":"connection_ack"}`) return;
-        server.close();
-        client.end();
-        resolve(
-          assert.deepStrictEqual(
-            chunk,
-            `{"type":"error","id":1,"payload":{"errors":[{"message":"Not a subscription operation"}]}}`
-          )
-        );
+        const json = JSON.parse(chunk);
+        if (json.type === `data`) {
+          assert.deepStrictEqual(json, {
+            type: 'data',
+            id: 1,
+            payload: { data: { addNotification: { message: 'Hello World' } } },
+          });
+          resolved = true;
+        }
+        if (json.type === 'complete' && resolved === true) {
+          // It should complete the subscription immediately since it is a mutations/queries
+          resolve();
+        }
+        return;
       });
     });
   });
@@ -301,21 +362,20 @@ describe('graphyne-ws: wsHandler', () => {
     );
     await new Promise((resolve, reject) => {
       client.on('data', (chunk) => {
-        if (chunk === `{"type":"connection_ack"}`) return;
-        server.close();
-        client.end();
-        const {
-          payload: {
-            errors: [{ message }],
-          },
-        } = JSON.parse(chunk);
-
-        resolve(
-          assert.deepStrictEqual(
+        const json = JSON.parse(chunk);
+        if (json.type === 'data') {
+          const {
+            payload: {
+              errors: [{ message }],
+            },
+          } = json;
+          assert.deepEqual(
             message,
             `Cannot query field "NNotificationAdded" on type "Subscription". Did you mean "notificationAdded"?`
-          )
-        );
+          );
+          resolve();
+          // FIXME: Add test for Subscription is stopped after this
+        }
       });
     });
   });
@@ -352,21 +412,10 @@ describe('graphyne-ws: wsHandler', () => {
               type: 'stop',
             })
           );
-          request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-              query: `
-            mutation {
-              addNotification(message: "Hello World") {
-                message
-              }
-            }
-          `,
-            })
-            .then(() => {
-              // Wait for little bit more to see if there is notification
-              timer = setTimeout(resolve, 20);
-            });
+          sendMessageMutation().then(() => {
+            // Wait for little bit more to see if there is notification
+            timer = setTimeout(resolve, 20);
+          });
         }
         if (data.type === 'data') {
           // We have unsubscribed, there should not be data
@@ -374,14 +423,12 @@ describe('graphyne-ws: wsHandler', () => {
           reject();
         }
       });
-    }).finally(() => {
-      client.end();
-      server.close();
     });
   });
   it('closes connection on error in context function', (done) => {
     const context = ({ connectionParams }) => {
-      if (connectionParams?.unauthenticated) return false;
+      if (connectionParams?.unauthenticated)
+        throw new Error('You must be authenticated!');
       return {};
     };
     startServer({}, {}, { context }).then(({ server, client }) => {
@@ -397,11 +444,9 @@ describe('graphyne-ws: wsHandler', () => {
       client.on('data', (chunk) => {
         isErrored =
           chunk ===
-          `{"type":"connection_error","payload":{"errors":[{"message":"Prohibited connection!"}]}}`;
+          `{"type":"connection_error","payload":{"errors":[{"message":"You must be authenticated!"}]}}`;
       });
       client.on('end', () => {
-        client.end();
-        server.close();
         done(assert(isErrored));
       });
     });
@@ -421,8 +466,6 @@ describe('graphyne-ws: wsHandler', () => {
         );
       });
       client.on('end', () => {
-        client.end();
-        server.close();
         done();
       });
     });
@@ -444,9 +487,6 @@ describe('graphyne-ws: SubscriptionConnection', () => {
                 resolve();
               } catch (e) {
                 reject(e);
-              } finally {
-                client.end();
-                server.close();
               }
             });
           },
@@ -493,9 +533,6 @@ describe('graphyne-ws: SubscriptionConnection', () => {
             resolve();
           } catch (e) {
             reject(e);
-          } finally {
-            client.end();
-            server.close();
           }
         });
       }
@@ -525,9 +562,6 @@ describe('graphyne-ws: SubscriptionConnection', () => {
             resolve();
           } catch (e) {
             reject(e);
-          } finally {
-            client.end();
-            server.close();
           }
         });
       }
@@ -577,8 +611,6 @@ describe('graphyne-ws: SubscriptionConnection', () => {
       function onSubscriptionConnection(connection: SubscriptionConnection) {
         connection.on('connection_terminate', () => {
           resolve();
-          client.end();
-          server.close();
         });
       }
       client.write(
