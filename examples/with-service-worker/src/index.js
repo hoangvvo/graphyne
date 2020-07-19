b27b622272c20fc66ba0ee242c6b9572ebd9611e
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-07-19 17:00:40 -0400

    Make example better

--- examples/with-service-worker/src/index.js
+++ examples/with-service-worker/src/index.js
@@ -11,32 +11,51 @@ if ('serviceWorker' in navigator) {
     .catch(function (error) {
       console.log('Service worker registration failed, error:', error);
     });
+
+  self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting()); // Activate worker immediately
+  });
+  
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim()); // Become available to all pages
+  });
 }
 
 window.onload = () => {
   function printResult(json, duration) {
-    document.querySelector('#result').value = JSON.stringify(
-      json,
-      null,
-      '  '
-    );
+    const value = typeof json === 'object' ? JSON.stringify(json, null, '  ') : json;
+    document.querySelector('#result').value = value;
     document.querySelector('#result-ms').innerText = duration.toFixed();
   }
   function resetResult() {
     document.querySelector('#result').value = null;
   }
+  // Via fetch
   document.querySelector('#queryFetch').onclick = async () => {
     resetResult();
     const t0 = performance.now();
     const res = await fetch('/graphql?query={hello}');
-    const t1 = performance.now();
-    printResult(await res.json(), t1 - t0);
+    printResult(await res.json(), performance.now() - t0);
   };
+  // Via urql
   document.querySelector('#queryUrql').onclick = async () => {
     resetResult();
     const t0 = performance.now();
     const result = await urqlClient.query('{ hello }').toPromise();
-    const t1 = performance.now();
-    printResult({ data: result.data }, t1 - t0);
+    printResult({ data: result.data }, performance.now() - t0);
   };
+  // Via service worker postMessage
+  
+  document.querySelector('#queryMessage').onclick = () => {
+    let t0 = 0;
+    const listenToResult = (event) => {
+      printResult(event.data, performance.now() - t0);
+      navigator.serviceWorker.removeEventListener('message', listenToResult);
+    }
+    t0 = performance.now();
+    navigator.serviceWorker.addEventListener('message', listenToResult);
+    navigator.serviceWorker.controller.postMessage({
+      query: '{ hello }'
+    });
+  }
 };
