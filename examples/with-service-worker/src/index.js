9debf473dc5277f0e9228f64ccf58abacd98a24d
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-07-19 16:13:33 -0400

    Add urql example

--- examples/with-service-worker/src/index.js
+++ examples/with-service-worker/src/index.js
@@ -1,3 +1,7 @@
+import { createClient } from '@urql/core';
+
+const urqlClient = createClient({ url: '/graphql' });
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/worker.js')
@@ -10,15 +14,29 @@ if ('serviceWorker' in navigator) {
 }
 
 window.onload = () => {
-  document.querySelector('#query').onclick = () => {
-    fetch('/graphql?query={hello}')
-      .then((res) => res.json())
-      .then((json) => {
-        document.querySelector('#result').value = JSON.stringify(
-          json,
-          null,
-          '\t'
-        );
-      });
+  function printResult(json, duration) {
+    document.querySelector('#result').value = JSON.stringify(
+      json,
+      null,
+      '  '
+    );
+    document.querySelector('#result-ms').innerText = duration.toFixed();
+  }
+  function resetResult() {
+    document.querySelector('#result').value = null;
+  }
+  document.querySelector('#queryFetch').onclick = async () => {
+    resetResult();
+    const t0 = performance.now();
+    const res = await fetch('/graphql?query={hello}');
+    const t1 = performance.now();
+    printResult(await res.json(), t1 - t0);
+  };
+  document.querySelector('#queryUrql').onclick = async () => {
+    resetResult();
+    const t0 = performance.now();
+    const result = await urqlClient.query('{ hello }').toPromise();
+    const t1 = performance.now();
+    printResult({ data: result.data }, t1 - t0);
   };
 };
